7f2721c6b89748c7d6249ead755d6d960e8f2e13
SergArtuh <artuh92@gmail.com>
2020-01-03 01:10:04 +0200

    Add window system module - wnd

--- src/llr/api.cpp
+++ src/llr/api.cpp
@@ -12,13 +12,26 @@
 
 #define MAX_BUFFER_SIZE 2048
 
+#define GL_CHECK if(glGetError() != GL_NO_ERROR) {LLR_ERROR("OpenGL error with error code %x, at %d line %s", glGetError(), __LINE__, __FILE__);}
+
 
 namespace llr {
 
-	void debugMsg(const int line, const char* const file, const char* str, const char * msgType, va_list args) {
+	llr::llr(wnd::Window& window)
+	{
+		window.makeContextCurrent();
+
+		glewInit();
+		GL_CHECK
+	}
+
+
+
+
+	void llrDebugMsg(const char* str, const char * msgType, va_list args) {
 		std::vector<char> buffer(MAX_BUFFER_SIZE);
 
-		snprintf(buffer.data(), buffer.size(), "[ LLR %s ] at line: %d, file %s:\t", msgType, line, file);
+		snprintf(buffer.data(), buffer.size(), "[ LLR %s ]\t", msgType);
 
 		const size_t shift = strlen(buffer.data());
 
@@ -28,24 +41,24 @@ namespace llr {
 		OutputDebugString("\n");
 	}
 
-	void errorMsg(const int line, const char* const file, const char* str, ...) {
+	void llrError(const char* str, ...) {
 		va_list args;
 		va_start(args, str);
-		debugMsg(line, file, str, "Error", args);
+		llrDebugMsg(str, "Error", args);
 		va_end(args);
 	}
 
-	void warningMsg(const int line, const char* const file, const char* str, ...) {
+	void llrWarning(const char* str, ...) {
 		va_list args;
 		va_start(args, str);
-		debugMsg(line, file, str, "Warning", args);
+		llrDebugMsg(str, "Warning", args);
 		va_end(args);
 	}
 
-	void infoMsg(const int line, const char* const file, const char* str, ...) {
+	void llrInfo(const char* str, ...) {
 		va_list args;
 		va_start(args, str);
-		debugMsg(line, file, str, "Info", args);
+		llrDebugMsg(str, "Info", args);
 		va_end(args);
 	}
 }
--- src/llr/api.h
+++ src/llr/api.h
@@ -1,20 +1,25 @@
 #pragma once
 
+#include "wnd/window.h"
+
 #define LLR_EXPORT __declspec(dllexport)
 
-#define LLR_ERROR(str, ...) llr::errorMsg( __LINE__, __FILE__, (str), __VA_ARGS__)
-#define LLR_WARNING(str, ...) llr::warningMsg( __LINE__, __FILE__, (str), __VA_ARGS__)
-#define LLR_INFO(str, ...) llr::infoMsg( __LINE__, __FILE__, (str), __VA_ARGS__)
+#define LLR_ERROR(str, ...) llrError((str), __VA_ARGS__)
+#define LLR_WARNING(str, ...) llrWarning((str), __VA_ARGS__)
+#define LLR_INFO(str, ...) llrInfo((str), __VA_ARGS__)
 
-#define GL_CHECK { GLenum err = glGetError(); if(err != GL_NO_ERROR) {LLR_ERROR("OpenGL error with error code %x, at %d line %s", err, __LINE__, __FILE__);}}
-
-#define UNUSED -1
+namespace wnd {
+	class Window;
+}
 
 namespace llr {
 
-	void errorMsg(const int line, const char * const file, const const char* str, ...);
+	void llrError(const char* str, ...);
+	void llrWarning(const char* str, ...);
+	void llrInfo(const char* str, ...);
 
-	void warningMsg(const int line, const char* const file, const char* str, ...);
-
-	void infoMsg(const int line, const char* const file, const char* str, ...);
+	class LLR_EXPORT llr {
+		public:
+			llr(wnd::Window & window);
+	};
 }
